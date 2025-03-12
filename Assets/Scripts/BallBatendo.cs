655003852b7e46cf69c9776a23112ae0f84639fe
Rotcivosk <victormonte43@gmail.com>
2025-03-11 21:25:04 -0400

    upd balls shaders

--- Assets/Scripts/BallBatendo.cs
+++ Assets/Scripts/BallBatendo.cs
@@ -2,34 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallBatendo : MonoBehaviour
+public class BallCollisionSounds : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource; // Fonte de áudio para reproduzir os sons
     [SerializeField] private AudioClip[] ballCollisionSounds; // Lista de sons para colisão com outras bolas (Tag: "Ball")
     [SerializeField] private AudioClip[] wallCollisionSounds; // Lista de sons para colisão com a parede (Tag: "Wall")
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ball"))
-        {
-            // Toca um som da lista de colisão com outras bolas
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Ball")) {            // Toca um som da lista de colisão com outras bolas
             PlayRandomSound(ballCollisionSounds);
+            return;
         }
-        else if (collision.gameObject.CompareTag("Wall"))
-        {
-            // Toca um som da lista de colisão com a parede
+        if (collision.gameObject.CompareTag("Wall")) { // Toca um som da lista de colisão com a parede
             PlayRandomSound(wallCollisionSounds);
+            return;
         }
     }
 
-    private void PlayRandomSound(AudioClip[] soundList)
-    {
+    private void PlayRandomSound(AudioClip[] soundList)    {
         // Verifica se a lista de sons não está vazia
-        if (soundList.Length > 0)
-        {
-            // Escolhe um som aleatório da lista
+        if (soundList.Length > 0) {
+            // Escolhe um som aleatório da lista e toca
             AudioClip randomSound = soundList[Random.Range(0, soundList.Length)];
-            // Toca o som escolhido
             audioSource.PlayOneShot(randomSound);
         }
     }
